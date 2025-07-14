00ff9932fc2345c4dafae74bcf0d2482e52f1bba
Ashutosh Kuinkel <108271619+hejjwheh@users.noreply.github.com>
2025-07-14 20:38:32 +1000

    register user created

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,6 +1,23 @@
-
+import { Request, Response, NextFunction } from 'express';
+import User from '../models/user.model';
 //register 
 
+export const register = async(req:Request,res:Response,next:NextFunction)=>{
+  try{
+    const {firstName,lastName,email,password,phone} = req.body;
+    const user = await User.create({firstName,lastName,email,password,phone});
+    res.status(201).json({
+      message: 'User registered successfully',
+      status: 'success',
+      success: true,
+      data: user
+    });
+  }
+  catch(err){
+    next(err);
+  }
+
+}
 
 //login
 

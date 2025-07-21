c764ac779f25eb462ac8a8db764d2cfdfb111282
Ashutosh Kuinkel <108271619+hejjwheh@users.noreply.github.com>
2025-07-21 20:39:45 +1000

    JWT authentication

---
+++ src/utils/jwt.utils.ts
@@ -0,0 +1,12 @@
+import jwt from "jsonwebtoken"
+import { IJWTPayload } from "../types/global.types"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const JWT_SECRET = process.env.JWT_SECRET ?? ''
+const JWT_EXPIRE_IN = process.env.JWT_EXPIRE_IN
+
+export const generateToken = (payload:IJWTPayload)=>{
+  return jwt.sign(payload,JWT_SECRET,{expiresIn:JWT_EXPIRE_IN as any})
+}

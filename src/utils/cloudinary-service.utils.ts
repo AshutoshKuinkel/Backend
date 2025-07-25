2400229f32554fdfc5eb220a3b310f1851b0d322
Ashutosh Kuinkel <108271619+hejjwheh@users.noreply.github.com>
2025-07-25 20:50:42 +1000

    multer+cloudinary

---
+++ src/utils/cloudinary-service.utils.ts
@@ -0,0 +1,25 @@
+import cloudinary from "../config/cloudinary.config";
+import CustomError from "../middlewares/error-handler.middleware";
+import fs from "fs";
+
+export const uploadFile = async(path:string,dir='/') =>{
+  try{
+    const {public_id,secure_url} = await cloudinary.uploader.upload(path,{
+      unique_filename:true,
+      folder:'MERN_CLASS_PROJECT'+ dir,
+      // allowed_formats:[]
+    })
+
+    //delete image from uploads
+    if(fs.existsSync(path)){
+      fs.unlinkSync(path)
+    }
+
+    return{
+      public_id,
+      path:secure_url
+    }
+  }catch{
+    throw new CustomError(`Error uploading file.`,500)
+  }
+}

1a05da80a12ddb75dfee46aa88b357283def33af
Ashutosh Kuinkel <108271619+hejjwheh@users.noreply.github.com>
2025-07-31 19:38:42 +1000

    Wishlist API

--- src/routes/wishlist.routes.ts
+++ src/routes/wishlist.routes.ts
@@ -1,10 +1,11 @@
 import express from "express";
-import { registerProductToWishlist, removeProductFromWishlist, getAllWishlistItems, checkIfProductInWishlist } from "../controllers/wishlist.controller";
+import { registerProductToWishlist, removeProductFromWishlist, getAllWishlistItems, checkIfProductInWishlist, clearWishlist } from "../controllers/wishlist.controller";
 const router = express.Router();
 
 router.post("/registerProductToWishlist", registerProductToWishlist);
 router.delete("/removeProductFromWishlist/:id", removeProductFromWishlist);
 router.get("/wishlist", getAllWishlistItems);
 router.get("/checkIfProductInWishlist/:productId", checkIfProductInWishlist);
+router.delete("/clearWishList",clearWishlist)
 
 export default router;

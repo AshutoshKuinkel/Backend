19922c8a10f78027d0e457f752f947bd36fc24e2
Ashutosh Kuinkel <108271619+hejjwheh@users.noreply.github.com>
2025-08-01 18:55:49 +1000

    Cart API done

--- src/controllers/cart.controller.ts
+++ src/controllers/cart.controller.ts
@@ -4,7 +4,7 @@ import CustomError from "../middlewares/error-handler.middleware";
 
 //we need CRUD model for Cart: register/add product to cart, get users cart items, update product quantity, remove select product from cart, remove all product from cart.
 
-const addProductToCart  = async(req:Request,res:Response,next:NextFunction)=>{
+export const addProductToCart  = async(req:Request,res:Response,next:NextFunction)=>{
   try{
     const {productId} = req.body
     const userId = req.user._id
@@ -29,7 +29,7 @@ const addProductToCart  = async(req:Request,res:Response,next:NextFunction)=>{
   }
 }
 
-const getUserCart  = async(req:Request,res:Response,next:NextFunction)=>{
+export const getUserCart  = async(req:Request,res:Response,next:NextFunction)=>{
   try{
     const userId = req.user._id;
     
@@ -46,25 +46,71 @@ const getUserCart  = async(req:Request,res:Response,next:NextFunction)=>{
   }
 }
 
-const updateCart  = async(req:Request,res:Response,next:NextFunction)=>{
+export const updateCart  = async(req:Request,res:Response,next:NextFunction)=>{
   try{
+    const { productId } = req.params;
+    const { quantity } = req.body;
+    const userId = req.user._id;
 
+    if (!quantity || quantity <= 0) {
+      throw new CustomError("Quantity must be greater than zero", 400);
+    }
+
+    const cartItem = await Cart.findOneAndUpdate(
+      { _id: productId, user: userId },
+      { quantity },
+      { new: true }
+    );
+
+    if (!cartItem) {
+      throw new CustomError("Cart item not found", 404);
+    }
+
+    res.status(200).json({
+      message: "Cart item updated successfully",
+      status: "success",
+      success: true,
+      data: cartItem,
+    });
   }catch(err){
     next(err)
   }
 }
 
-const removeItemFromCart  = async(req:Request,res:Response,next:NextFunction)=>{
+export const removeProductFromCart  = async(req:Request,res:Response,next:NextFunction)=>{
   try{
-    
+    const { productId } = req.params;
+    const userId = req.user._id;
+
+    const cartItem = await Cart.findOneAndDelete({ _id: productId, user: userId });
+
+    if (!cartItem) {
+      throw new CustomError("Cart item not found", 404);
+    }
+
+    res.status(200).json({
+      message: "Cart item removed successfully",
+      status: "success",
+      success: true,
+      data: cartItem,
+    });
   }catch(err){
     next(err)
   }
 }
 
-const clearCart  = async(req:Request,res:Response,next:NextFunction)=>{
+export const clearCart  = async(req:Request,res:Response,next:NextFunction)=>{
   try{
+    const userId = req.user._id;
 
+    const cartItems = await Cart.deleteMany({ user: userId });
+
+    res.status(200).json({
+      message: "Cart cleared successfully",
+      status: "success",
+      success: true,
+      data: cartItems,
+    });
   }catch(err){
     next(err)
   }
